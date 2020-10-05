343e1da0a5bea527a3372906a5ed4a47ceaae7d9
Roman <65686226+morozit@users.noreply.github.com>
2020-10-06 00:21:46 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,13 +1,48 @@
-'use strict';
+console.log("=======>task-6<=======");
+// !  Variant-1
 
-const inputRef = document.querySelector('#validation-input');
+("use strict");
 
-inputRef.addEventListener('focus', e => {
-  inputRef.classList.remove('invalid', 'valid');
+const inputRef = document.querySelector("#validation-input");
+
+inputRef.addEventListener("focus", (e) => {
+  inputRef.classList.remove("invalid", "valid");
 });
 
-inputRef.addEventListener('blur', e => {
-  e.target.value.length >= e.target.getAttribute('data-length')
-    ? inputRef.classList.add('valid')
-    : inputRef.classList.add('invalid');
+inputRef.addEventListener("blur", (e) => {
+  e.target.value.length == e.target.getAttribute("data-length")
+    ? inputRef.classList.add("valid")
+    : inputRef.classList.add("invalid");
 });
+
+// !  Variant-2
+
+// let strangeInputRef = document.querySelector("#validation-input");
+// strangeInputRef.addEventListener("blur", () => {
+//   if (strangeInputRef.value.length < +strangeInputRef.dataset.length) {
+//     strangeInputRef.classList.remove("valid");
+//     strangeInputRef.classList.add("invalid");
+//   } else {
+//     strangeInputRef.classList.remove("invalid");
+//     strangeInputRef.classList.add("valid");
+//   }
+// });
+
+// ! Variant-3
+// const inputRef = document.querySelector("#validation-input");
+// console.log(inputRef);
+// const dataRef = document.querySelector("#validation-input").dataset.length;
+
+// function onInputChange(event) {
+//   if (Number(event.currentTarget.value.length) === Number(dataRef)) {
+//     console.log("yes");
+
+//     inputRef.classList.add("valid");
+//   } else {
+//     console.log("no");
+
+//     inputRef.classList.add("invalid");
+//   }
+// }
+
+// inputRef.addEventListener("blur", onInputChange);
