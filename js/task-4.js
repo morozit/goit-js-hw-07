405a7f858938253183dd3c3f8f24a907077a2b05
Roman <65686226+morozit@users.noreply.github.com>
2020-10-06 00:21:41 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,19 @@
-'use strict';
+console.log("=======>task-4<=======");
 
-const btnDecrementRef = document.querySelector('button[data-action="decrement"]');
-const btnIncrementRef = document.querySelector('button[data-action="increment"]');
+// !  Variant-1
+("use strict");
 
-const valueRef = document.querySelector('#value');
+const btnDecrementRef = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const btnIncrementRef = document.querySelector(
+  'button[data-action="increment"]'
+);
 
-btnDecrementRef.addEventListener('click', decrement);
-btnIncrementRef.addEventListener('click', increment);
+const valueRef = document.querySelector("#value");
+
+btnDecrementRef.addEventListener("click", decrement);
+btnIncrementRef.addEventListener("click", increment);
 
 let counterValue = 0;
 
@@ -19,3 +26,19 @@ function increment() {
   counterValue += 1;
   valueRef.textContent = counterValue;
 }
+
+// !  Variant-2
+
+// let counterPlus = document.querySelector("[data-action=increment]");
+// let counterMinus = document.querySelector("[data-action=decrement]");
+// let counterSpan = document.querySelector("#value");
+// let counterValue = 0;
+// const increment = () => {
+//   counterValue = ++document.querySelector("#value").textContent;
+// };
+// const decrement = (event) => {
+//   if (counterValue > 0)
+//     counterValue = --document.querySelector("#value").textContent;
+// };
+// counterPlus.addEventListener("click", increment);
+// counterMinus.addEventListener("click", decrement);
