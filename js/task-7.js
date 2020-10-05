8337c19d1c4bef1325c815a083d51c82e4f7cbe3
Roman <65686226+morozit@users.noreply.github.com>
2020-10-06 00:21:48 +0300

    Update task-7.js

--- js/task-7.js
+++ js/task-7.js
@@ -1,8 +1,21 @@
-'use strict';
+console.log("=======>task-7<=======");
 
-const rangeRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
+// !  Variant-1
+("use strict");
 
-rangeRef.addEventListener('input', e => {
+const rangeRef = document.querySelector("#font-size-control");
+const textRef = document.querySelector("#text");
+
+rangeRef.addEventListener("input", (e) => {
   textRef.style.fontSize = `${e.target.value}px`;
 });
+
+// !  Variant-2
+// let rangeRef = document.querySelector("#font-size-control");
+// let textRef = document.querySelector("#text");
+
+// textRef.style.fontSize = "50px";
+// rangeRef.addEventListener(
+//   "input",
+//   () => (textRef.style.fontSize = rangeRef.value + "px")
+// );
