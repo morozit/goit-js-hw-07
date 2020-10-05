72ad325949aeb842e528b40fa81abf0f4234ecf6
Roman <65686226+morozit@users.noreply.github.com>
2020-10-06 00:21:34 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,42 @@
-'use strict';
+console.log("=======>task-2<=======");
 
-const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+// !  Variant-1
 
-const ingredientsRef = document.querySelector('#ingredients');
+// ("use strict");
 
-const arr = ingredients.map(ingredient => {
-  const createElement = document.createElement('li');
-  createElement.textContent = ingredient;
-  return createElement;
+// const ingredients = [
+//   "Картошка",
+//   "Грибы",
+//   "Чеснок",
+//   "Помидоры",
+//   "Зелень",
+//   "Приправы",
+// ];
+
+// const ingredientsRef = document.querySelector("#ingredients");
+
+// const arr = ingredients.map((ingredient) => {
+//   const createElement = document.createElement("li");
+//   createElement.textContent = ingredient;
+//   return createElement;
+// });
+
+// ingredientsRef.append(...arr);
+
+// !  Variant-2
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+const ingredientsListRef = document.querySelector("#ingredients");
+let newListItem = ingredients.map((el, index, arr) => {
+  arr[index] = document.createElement("li");
+  arr[index].textContent = el;
+  return arr[index];
 });
-
-ingredientsRef.append(...arr);
+ingredientsListRef.append(...newListItem);
