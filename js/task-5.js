0ce370bd69d88caf5ab546d7f8e6c33030419119
Roman <65686226+morozit@users.noreply.github.com>
2020-10-06 00:21:43 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,12 +1,26 @@
-'use strict';
+console.log("=======>task-5<=======");
+// !  Variant-1
 
-const inputRef = document.querySelector('#name-input');
-const titleSpanRef = document.querySelector('#name-output');
+// ("use strict");
 
-inputRef.addEventListener('input', e => {
-  titleSpanRef.textContent = e.target.value;
+// const inputRef = document.querySelector("#name-input");
+// const titleSpanRef = document.querySelector("#name-output");
 
-  e.target.value
-    ? (titleSpanRef.textContent = e.target.value)
-    : (titleSpanRef.textContent = 'незнакомец');
-});
+// inputRef.addEventListener("input", (e) => {
+//   titleSpanRef.textContent = e.target.value;
+
+//   e.target.value
+//     ? (titleSpanRef.textContent = e.target.value)
+//     : (titleSpanRef.textContent = "незнакомец");
+// });
+
+// !  Variant-2
+
+let inputRef = document.querySelector("#name-input");
+let outputNameRef = document.querySelector("#name-output");
+inputRef.addEventListener(
+  "input",
+  () =>
+    (outputNameRef.textContent =
+      inputRef.value.length > 0 ? inputRef.value : "незнакомец")
+);
